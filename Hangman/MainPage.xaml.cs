68d130a9528293161e36154916261a014bd3e6c0
Lucas Araujo <lsa77793@gmail.com>
2025-07-23 23:18:50 -0300

    Refactor navegación y amplía funcionalidad de MainPage

--- Hangman/MainPage.xaml.cs
+++ Hangman/MainPage.xaml.cs
@@ -2,14 +2,32 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        #region Fields  
+        List<string> words = new List<string>()
+           {
+               "python",
+               "javascript",
+               "maui",
+               "csharp",
+               "mongodb",
+               "sql",
+               "xaml",
+               "word",
+               "excel",
+               "powerpoint",
+               "code",
+               "hotreload",
+               "snippets"
+           };
+        string answer = "";
+
+
+        #endregion
 
         public MainPage()
         {
             InitializeComponent();
         }
-
-       
     }
 
 }
