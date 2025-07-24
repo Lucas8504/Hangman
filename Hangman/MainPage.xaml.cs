e8b3573df0c49ca9d5cf00d84a512b0988b38901
Lucas Araujo <lsa77793@gmail.com>
2025-07-24 18:47:44 -0300

    Añadir soporte para INotifyPropertyChanged en MainPage

--- Hangman/MainPage.xaml.cs
+++ Hangman/MainPage.xaml.cs
@@ -1,33 +1,72 @@
-﻿namespace Hangman
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Hangman
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
+        #region UI Properties
+        public string Spotlight
+        {
+            get => spotlight;
+            set
+            {
+                spotlight = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
         #region Fields  
         List<string> words = new List<string>()
-           {
-               "python",
-               "javascript",
-               "maui",
-               "csharp",
-               "mongodb",
-               "sql",
-               "xaml",
-               "word",
-               "excel",
-               "powerpoint",
-               "code",
-               "hotreload",
-               "snippets"
-           };
+                   {
+                       "python",
+                       "javascript",
+                       "maui",
+                       "csharp",
+                       "mongodb",
+                       "sql",
+                       "xaml",
+                       "word",
+                       "excel",
+                       "powerpoint",
+                       "code",
+                       "hotreload",
+                       "snippets"
+                   };
         string answer = "";
-
+        private string spotlight = string.Empty; // Initialize to avoid nullability warning
+        List<char> guessed = new List<char>();
 
         #endregion
 
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+            PickWord();
+            CalculateWord(answer, guessed);
         }
+
+        #region Game Engine
+        private void PickWord()
+        {
+            answer =
+                 words[new Random().Next(0, words.Count)];
+            Debug.WriteLine(answer);
+        }
+
+        private void CalculateWord(string answer, List<char> guessed)
+        {
+            var temp =
+                 answer.Select(x => (guessed.IndexOf(x) >= 0 ? x : '_'))
+                 .ToArray();
+            Spotlight = string.Join(' ', temp);
+
+        }
+
+        #endregion
     }
 
 }
